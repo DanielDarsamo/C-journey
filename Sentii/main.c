2017a30d8d14bfdbc935de95498cb375b9d98e57
Daniel <28514526+astrobeta@users.noreply.github.com>
2024-11-08 23:33:03 +0200

    update

---
+++ Sentii/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int user;
+    int option; 
+    char name [20];
+    int luckynumber=9;
+    int counter=3; 
+    
+    printf("Hello, welcome to!\n");
+    return 0;
+}
--- rainbow/main.c
+++ rainbow/main.c
@@ -1,31 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+// violet, green, yellow, orange, indigo, red, blue
 
 int main()
 {
-    char *rainbow[]={"blue","green","red","orange","purple","yellow"};
-    char choice[3];
-    char name[15];
-    int ramb=6; 
-    printf("welcome to guessing your rainbow color\n");
-    printf("whats your name ? \n :");
-    gets(name);
-    printf("hello %s \n",name);
-    printf("Lets begin \n");
-    printf("you can only choose one color, by answering yes or no \n");
-     for(int i=0; i<6; i++)
-     {  printf(" Is %s your favorite rainbow color \n",rainbow[i]);
-            gets(choice);
-       if(choice=="no"){
-            ramb--;
-            printf ("you have %d colors left ",ramb);
-       }
-        else if (choice=="yes"){
-            printf("YAY");
-            return 0;
-       
-        }
-     }
+
+
         
     return 0;
 }
